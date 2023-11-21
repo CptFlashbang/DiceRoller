be77cb7fbe5b2a0bd591df9f5416b209653d354d
Callum Yates <y042944m@student.staffs.ac.uk>
2023-11-21 11:04:19 +0000

    Imported relevent classses added string resource for button text added image-button composable made button clickable

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -18,26 +25,72 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                DiceApp(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun DiceApp() {
+    var roll by remember { mutableStateOf((1..6).random()) }
+
+    var imageResource = when (roll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    DiceImageAndButton(
+        imageResource = imageResource,
+        contentDescriptor = roll,
+        modifier = Modifier,
+        onClick = { roll = (1..6).random() }
     )
+
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    DiceRollerTheme {
-        Greeting("Android")
+fun DiceImageAndButton(
+    imageResource: Int,
+    contentDescriptor: Int,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = contentDescriptor.toString()
+        )
+        Button(onClick = onClick) {
+            Text(text = stringResource(R.string.roll_dice))
+        }
     }
 }
+
+
+
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DiceRollerTheme {
+//        Greeting("Android")
+//    }
+//}
